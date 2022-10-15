d5d3f591fd5d123939eca95792d78adc52360f55
metinmogensen <70538201+metinmogensen@users.noreply.github.com>
2022-10-15 16:12:29 +0200

    First screen setup

--- App.js
+++ App.js
@@ -1,26 +1,35 @@
-import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, StatusBar, View } from 'react-native';
-import DrivingSchoolScreen from './src/features/screens/drivingSchool.screen';
-
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StatusBar,
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  View,
+  Platform,
+} from "react-native";
 
 export default function App() {
   return (
-    <>    
-    <View>
-      <DrivingSchoolScreen />  
-      </View>
-    <ExpoStatusBar style='auto'/>
+    <>
+      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+        <View style={{ padding: 16, backgroundColor: "green" }}>
+          <Text>search</Text>
+        </View>
+        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
     </>
-
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'black',
-    marginTop: StatusBar.currentHeight
-
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
