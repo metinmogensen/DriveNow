7d3ddc4c655546fbeaa11bf7a6ba04f4415f6cf2
metinmogensen <70538201+metinmogensen@users.noreply.github.com>
2022-10-16 13:19:25 +0200

    Setup tabNagivation, next is implementing map

--- App.js
+++ App.js
@@ -3,12 +3,31 @@ import React from "react";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
 import { DrivingSchoolScreen } from "./src/features/drivingSchools/screens/drivingSchool.screen";
-
 import {
   useFonts as useOswald,
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+
+function MAP() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function Settings() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -26,9 +45,33 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <DrivingSchoolScreen theme={theme} />
-        <ExpoStatusBar style="auto" />
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ color, size }) => {
+                let iconName;
+
+                if (route.name === "Skoler") {
+                  iconName = "search-outline";
+                } else if (route.name === "Kort") {
+                  iconName = "map";
+                } else if (route.name === "Indstillinger") {
+                  iconName = "md-settings";
+                }
+                // You can return any component that you like here!
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+              tabBarActiveTintColor: "tomato",
+              tabBarInactiveTintColor: "gray",
+            })}
+          >
+            <Tab.Screen name="Skoler" component={DrivingSchoolScreen} />
+            <Tab.Screen name="Kort" component={MAP} />
+            <Tab.Screen name="Indstillinger" component={Settings} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
