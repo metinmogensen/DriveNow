2cfd42ad705536691c82a97c85524c1699629929
metinmogensen <70538201+metinmogensen@users.noreply.github.com>
2022-10-25 12:57:41 +0200

    Fixed anti pattern. (react with service layer pattern)

--- src/services/location/location.context.js
+++ src/services/location/location.context.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { locationRequest, locationTransform } from "./location.service";
 
 export const LocationContext = React.createContext();
 
 export const LocationContextProvider = ({ children }) => {
-  const [keyword, setKeyword] = useState("");
+  const [keyword, setKeyword] = useState("københavn");
   const [location, setLocation] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -13,10 +13,13 @@ export const LocationContextProvider = ({ children }) => {
   const onSearch = (searchKeyword) => {
     setIsLoading(true);
     setKeyword(searchKeyword);
-    if (!searchKeyword.length) {
+  };
+
+  useEffect(() => {
+    if (!keyword.length) {
       return;
     }
-    locationRequest(searchKeyword.toLowerCase())
+    locationRequest(keyword.toLowerCase())
       .then(locationTransform)
       .then((result) => {
         setIsLoading(false);
@@ -28,7 +31,7 @@ export const LocationContextProvider = ({ children }) => {
         setError(err);
         console.log(`"${searchKeyword}"` + " findes ikke");
       });
-  };
+  }, [keyword]);
 
   return (
     <LocationContext.Provider
