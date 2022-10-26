8381238dee07cd0969c5d0a27d54c8bfb491cc0c
metinmogensen <70538201+metinmogensen@users.noreply.github.com>
2022-10-26 13:03:04 +0200

    basic list is now done -> static accordions though

--- src/features/drivingSchools/screens/drivingSchoolDetails.screen.js
+++ src/features/drivingSchools/screens/drivingSchoolDetails.screen.js
@@ -1,5 +1,6 @@
-import React from "react";
-import { StatusBar, SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import { List } from "react-native-paper";
+import { StatusBar, SafeAreaView, ScrollView } from "react-native";
 import styled from "styled-components/native";
 import { DrivingschoolInfo } from "../components/drivingSchool-info.components";
 
@@ -9,10 +10,55 @@ const SafeArea = styled(SafeAreaView)`
 `;
 
 export const DrivingSchoolDetailScreen = ({ route }) => {
+  const [bKørekortExpanded, setBkørekortExpanded] = useState(false);
+  const [MotorcykelkortExpanded, setMotorcykelkortExpanded] = useState(false);
+  const [telefonExpanded, setTelefonExpanded] = useState(false);
+  const [emailExpanded, setEmailExpanded] = useState(false);
+
   const { drivingSchools } = route.params;
   return (
     <SafeArea>
       <DrivingschoolInfo drivingSchools={drivingSchools} />
+      <ScrollView>
+        <List.Accordion
+          title="Kategori B Kørekort"
+          left={(props) => <List.Icon {...props} icon="car-hatchback" />}
+          expanded={bKørekortExpanded}
+          onPress={() => setBkørekortExpanded(!bKørekortExpanded)}
+        >
+          <List.Item title="Lyn-kursus" />
+          <List.Item title="Alm. kursus" />
+        </List.Accordion>
+
+        <List.Accordion
+          title="Motorcykelkort"
+          left={(props) => <List.Icon {...props} icon="motorbike" />}
+          expanded={MotorcykelkortExpanded}
+          onPress={() => setMotorcykelkortExpanded(!MotorcykelkortExpanded)}
+        >
+          <List.Item title="Alm. kursus" />
+          <List.Item title="Lyn-kursus" />
+        </List.Accordion>
+
+        <List.Accordion
+          title="Kontakt via Telefon"
+          left={(props) => <List.Icon {...props} icon="phone" />}
+          expanded={telefonExpanded}
+          onPress={() => setTelefonExpanded(!telefonExpanded)}
+        >
+          <List.Item title="Ejer: 12312312" />
+          <List.Item title="Kontor: 89898989" />
+        </List.Accordion>
+
+        <List.Accordion
+          title="Kontakt Via E-mail"
+          left={(props) => <List.Icon {...props} icon="at" />}
+          expanded={emailExpanded}
+          onPress={() => setEmailExpanded(!emailExpanded)}
+        >
+          <List.Item title="memo19ad@gmail.com" />
+        </List.Accordion>
+      </ScrollView>
     </SafeArea>
   );
 };
